5a2c3d3c14cf179c85995afa397e4cf2af259589
agrawalameesha21 <agrawalameesha21@gmail.com>
2026-05-11 01:53:17 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const hashSet = [];
+        let i = 0;
+        while(i < nums.length) {
+            if(hashSet.includes(nums[i])) return true;
+            hashSet.push(nums[i]);
+            i++
+        }
+        return false;
+    }
+}
